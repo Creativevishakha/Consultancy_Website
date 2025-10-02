6690253f9092a011b0aa143f5387bcbbffa82f22
Vishakha Rathore <121350580+Creativevishakha@users.noreply.github.com>
2025-10-03 00:47:35 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Lawyer from './pages/Lawyer'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Myprofile from './pages/Myprofile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/'element={<Home/>}/>
+        <Route path='/Lawyer'element={<Lawyer/>}/>
+        <Route path='/Lawyer/:speciality'element={<Lawyer/>}/>
+        <Route path='/Login'element={<Login/>}/>
+        <Route path='/About'element={<About/>}/>
+        <Route path='/Contact'element={<Contact/>}/>
+        <Route path='/my-profile'element={<Myprofile/>}/>
+        <Route path='/my-Appointements'element={<MyAppointments/>}/>
+        {/* <Route path='/appointment/:LawyerId'element={<Appointment/>}/> */}
+        <Route path='/appointment/:docId' element={<Appointment />} />
+
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
